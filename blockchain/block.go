21ce4d973e73bfa7ab576a27ba160b9d7aa43810
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-04-14 21:48:58 +0900

    persisting the blockchain

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,15 +1,12 @@
 package blockchain
 
 import (
-	"bytes"
 	"crypto/sha256"
-	"encoding/gob"
 	"fmt"
 
 	"github.com/nomadcoders/nomadcoin/db"
 	"github.com/nomadcoders/nomadcoin/utils"
 )
-
 type Block struct {
 	Data     string `json:"data"`
 	Hash     string `json:"hash"`
@@ -17,15 +14,8 @@ type Block struct {
 	Height   int    `json:"height"`
 }
 
-func (b *Block) toBytes() []byte {
-	var blockBuffer bytes.Buffer
-	encoder := gob.NewEncoder(&blockBuffer)
-	utils.HandleErr(encoder.Encode(b))
-	return blockBuffer.Bytes()
-}
-
 func (b *Block) persist() {
-	db.SaveBlock(b.Hash, b.toBytes())
+	db.SaveBlock(b.Hash, utils.ToBytes(b))
 }
 
 func createBlock(data string, prevHash string, height int) *Block {
